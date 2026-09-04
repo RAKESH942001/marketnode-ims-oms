d710d15482bb46fc88b4ec45e44f65f6326a5e7b
RAKESH942001 <nlrakesh44@gmail.com>
2026-09-04 13:21:45 +0530

    feat: add custom exceptions for clean error handling

---
+++ backend/src/main/java/com/example/inventorymngt/exception/InsufficientStockException.java
@@ -0,0 +1,8 @@
+package com.example.inventorymngt.exception;
+
+public class InsufficientStockException {
+
+    public InsufficientStockException(String message) {
+        super(message);
+    }
+}
---
+++ backend/src/main/java/com/example/inventorymngt/exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.example.inventorymngt.exception;
+
+public class ResourceNotFoundException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
